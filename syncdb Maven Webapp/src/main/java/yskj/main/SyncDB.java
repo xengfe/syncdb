946eb409e2277d25da3e8f8486639c0f84dae851
admin <admin@DESKTOP-OL9ET3R>
2016-06-01 13:07:01 +0800

    增加定时任务

--- syncdb Maven Webapp/src/main/java/yskj/main/SyncDB.java
+++ syncdb Maven Webapp/src/main/java/yskj/main/SyncDB.java
@@ -1,11 +1,35 @@
 package yskj.main;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
 import yskj.util.DBManager;
 
 public class SyncDB {
 
 	public static void main(String[] args) {
-		DBManager.save2DistDB();
+		int delay = 0;//毫秒
+		int period = 1000*5;
+		
+		Timer timer = new Timer(); 
+		TimerTask task = new TimerTask (){
+
+			@Override
+			public void run() {
+				long beginTime = System.currentTimeMillis();
+				DBManager.save2DistDB();
+				long endTime =System.currentTimeMillis();
+				System.out.println("操作时长：" + (endTime - beginTime) +" 毫秒");
+				
+			}
+			
+		};
+		timer.schedule(task, delay,period) ;
+		
+		
+		
 	}
 
 }
